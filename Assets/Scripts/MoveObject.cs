1140f67b6b4b4d45e784262005a12e789648ea4c
Unknown <werner.felixx@gmx.de>
2018-06-23 22:06:36 +0200

    Felix_InGameMouse

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -1,103 +1,116 @@
 ﻿using UnityEngine;
 
-public class MoveObject : MonoBehaviour {
-	Rigidbody rb;
-	Camera mainCam;
+public class MoveObject : MonoBehaviour
+{
+    Rigidbody rb;
+    Camera mainCam;
 
-	public float raiseHeight = 0.1f;
+    public float raiseHeight = 0.1f;
 
-	bool mouseDragging = false;
-	bool mouseOverObject = false;
+    bool mouseDragging = false;
+    bool mouseOverObject = false;
 
-	public ObjectsSO objects;
+    public ObjectsSO objects;
 
-	[Header("Type of Object")]
-	public ObjectType objectType = ObjectType.MOUSE;
-	private Vector3 startPosition;
-	private Quaternion startRotation;
-	private GameObject cursor;
+    [Header("Type of Object")]
+    public ObjectType objectType = ObjectType.MOUSE;
+    public float moveBounds;
+    public Vector3 temp;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private GameObject cursor;
 
-	public enum ObjectType {
-		FREE,
-		MOUSE
-	};
+    public enum ObjectType
+    {
+        FREE,
+        MOUSE
+    };
 
-	// stuff einstellen
-	void Start () {
-		rb = GetComponent<Rigidbody>();
-		mainCam = Camera.main;
-		startPosition = transform.position;
-		startRotation = transform.rotation;
+    // stuff einstellen
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCam = Camera.main;
+        startPosition = transform.position;
+        startRotation = transform.rotation;
 
-		cursor = GameObject.Find("Cursor");
-	}
+        cursor = GameObject.Find("Cursor");
+    }
 
-	private void Update() {
-		// Objekt halten/ loslassen
-		if (mouseOverObject || mouseDragging) {
-			if (Input.GetButtonDown("Fire1")) {
-				mouseDragging = !mouseDragging;
-			}
-		}
+    private void Update()
+    {
+        // Objekt halten/ loslassen
+        if (mouseOverObject || mouseDragging)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                mouseDragging = !mouseDragging;
+            }
+        }
 
-		// objekt wird gehalten
-		if (mouseDragging) {
-			rb.useGravity = false;
+        // objekt wird gehalten
+        if (mouseDragging)
+        {
+            rb.useGravity = false;
 
-			RaycastHit hit;
-			Vector3 mousePos;
-			Ray mouseToWorldRay = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Vector3 mousePos;
+            Ray mouseToWorldRay = mainCam.ScreenPointToRay(Input.mousePosition);
 
-			// berechen wohin Object bewegt werden soll
-			if (Physics.Raycast(mouseToWorldRay, out hit, objects.maxDist, objects.collisionLayer)){
-				mousePos = hit.point;
-			} else {
-				mousePos = mouseToWorldRay.direction.normalized * objects.maxDist + mainCam.transform.position;
-			}
+            // berechen wohin Object bewegt werden soll
+            if (Physics.Raycast(mouseToWorldRay, out hit, objects.maxDist, objects.collisionLayer))
+            {
+                mousePos = hit.point;
+            }
+            else
+            {
+                mousePos = mouseToWorldRay.direction.normalized * objects.maxDist + mainCam.transform.position;
+            }
 
-			// Objekt hängt so nicht mehr im Boden fest
-			mousePos.y += GetComponent<BoxCollider>().size.y / 2 + raiseHeight;
+            // Objekt hängt so nicht mehr im Boden fest
+            mousePos.y += GetComponent<BoxCollider>().size.y / 2 + raiseHeight;
 
-			// bewege Objekt abhängig vom Typ
-			switch (objectType) {
-				case (ObjectType.FREE):
-					transform.position = mousePos;
-					break;
-				case (ObjectType.MOUSE):
-					// bewege Maus nur auf Oberfläche
-					if (hit.point != Vector3.zero) {
-						// bewege Maus
-						Vector3 nextPos = Vector3.ProjectOnPlane(mousePos, Vector3.up) + (startPosition.y * Vector3.up);
-						Vector3 movementDelta = nextPos - transform.position;
-						transform.position += movementDelta;
-						// bewege Cursor
-						cursor.transform.localPosition += new Vector3(movementDelta.x, movementDelta.z, 0) * objects.mouseSensitivityInGame;
+            // bewege Objekt abhängig vom Typ
+            switch (objectType)
+            {
+                case (ObjectType.FREE):
+                    transform.position = mousePos;
+                    break;
+                case (ObjectType.MOUSE):
+                    // bewege Maus nur auf Oberfläche
+                    if (hit.point != Vector3.zero)
+                    {
+                        // bewege Maus
+                        Vector3 dir = Vector3.ProjectOnPlane(mousePos - startPosition, Vector3.up);
+                        dir.x = Mathf.Clamp(dir.x, -moveBounds, moveBounds);
+                        dir.z = Mathf.Clamp(dir.z, -moveBounds, moveBounds);
+                        transform.position = dir + startPosition;
+                        // bewege Cursor
+                        cursor.transform.localPosition = new Vector3((dir.x/moveBounds) * (objects.canvasSize.x/2), (dir.z/moveBounds) * (objects.canvasSize.y/2), 0);
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
+    }
 
-						// clampe die inGame Maus auf Bildschirm
-						Vector2 boundaries;
-						boundaries.x = (objects.canvasSize.x / 2) - (cursor.GetComponent<RectTransform>().sizeDelta.x / 2);
-						boundaries.y = (objects.canvasSize.y / 2) - (cursor.GetComponent<RectTransform>().sizeDelta.x / 2);
-						cursor.transform.localPosition = new Vector3(Mathf.Clamp(cursor.transform.localPosition.x, -boundaries.x, boundaries.x),
-																	 Mathf.Clamp(cursor.transform.localPosition.y, -boundaries.y, boundaries.y), 0);
-					}
-					break;
-			}
-		} else {
-			rb.useGravity = true;
-		}
-	}
+    private void OnMouseEnter()
+    {
+        mouseOverObject = true;
+    }
 
-	private void OnMouseEnter() {
-		mouseOverObject = true;
-	}
+    private void OnMouseExit()
+    {
+        mouseOverObject = false;
+    }
 
-	private void OnMouseExit() {
-		mouseOverObject = false;
-	}
-
-	public void Reset() {
-		Debug.Log("RESET");
-		this.transform.position = startPosition;
-		this.transform.rotation = startRotation;
-	}
+    public void Reset()
+    {
+        Debug.Log(this.gameObject.name + " RESET");
+        this.transform.position = startPosition;
+        this.transform.rotation = startRotation;
+    }
 }
--- Assets/Scripts/ObjectsSO.cs
+++ Assets/Scripts/ObjectsSO.cs
@@ -8,7 +8,7 @@ public class ObjectsSO : ScriptableObject {
 	public float maxDist = 3.0f;
 	public LayerMask collisionLayer;
 
-	[Range(0,2)]
+	[Range(0,5)]
 	public float mouseSensitivityInGame = 1.0f;
 	public Vector2 canvasSize = new Vector2(4, 3);
 }
