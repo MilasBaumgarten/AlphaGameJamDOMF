cefc16c15b905a27d6b64ce5ab2a6cfa3538f28f
Unknown <werner.felixx@gmx.de>
2018-06-23 05:10:56 +0200

    Eventsystem, camera zoom, prefabs, and stuff

---
+++ Assets/Interaction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Interaction : MonoBehaviour {
+
+    public UnityEvent OnButtonPressed;
+    public string type = "";
+    public KeyCode key;
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("button pressed");
+            OnButtonPressed.Invoke();
+        }
+    }
+}
