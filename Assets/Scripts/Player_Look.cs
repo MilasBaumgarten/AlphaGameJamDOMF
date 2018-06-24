35169fea9d976069a529c63b6fbd9f56288f5c59
Unknown <werner.felixx@gmx.de>
2018-06-24 18:59:53 +0200

    stuff

--- Assets/Scripts/Player_Look.cs
+++ Assets/Scripts/Player_Look.cs
@@ -23,15 +23,19 @@ public class Player_Look : MonoBehaviour
 
     void Update()
     {
+        
         Vector2 pos = cam.ScreenToViewportPoint(Input.mousePosition);
 
-        if (pos.x <= screenBounds)
+        if (!zoomState)
         {
-            cam.transform.rotation = new Quaternion(cam.transform.rotation.x, Mathf.Clamp(cam.transform.rotation.y - rotaSpeed * Time.deltaTime, -lookBounds, lookBounds), cam.transform.rotation.z, cam.transform.rotation.w);
-        }
-        if (pos.x >= 1.0f - screenBounds)
-        {
-            cam.transform.rotation = new Quaternion(cam.transform.rotation.x, Mathf.Clamp(cam.transform.rotation.y + rotaSpeed * Time.deltaTime, -lookBounds, lookBounds), cam.transform.rotation.z, cam.transform.rotation.w);
+            if (pos.x <= screenBounds)
+            {
+                cam.transform.rotation = new Quaternion(cam.transform.rotation.x, Mathf.Clamp(cam.transform.rotation.y - rotaSpeed * Time.deltaTime, -lookBounds, lookBounds), cam.transform.rotation.z, cam.transform.rotation.w);
+            }
+            if (pos.x >= 1.0f - screenBounds)
+            {
+                cam.transform.rotation = new Quaternion(cam.transform.rotation.x, Mathf.Clamp(cam.transform.rotation.y + rotaSpeed * Time.deltaTime, -lookBounds, lookBounds), cam.transform.rotation.z, cam.transform.rotation.w);
+            }
         }
 
         cursor.transform.position = new Vector3(pos.x * Screen.width, pos.y * Screen.height, 0.0f);
@@ -43,6 +47,7 @@ public class Player_Look : MonoBehaviour
         if (zoomState)
         {
             cam.transform.position = zoomedInPosition.position;
+            cam.transform.rotation = Quaternion.identity;
         }
         else
         {
